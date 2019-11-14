5958bf15346850a48f671cafc46380ef23f61ce2
Jacob Fletcher <jacobsfletch@gmail.com>
2019-11-13 21:01:41 -0500

    exposes WindowInfoContext

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
+export { default as WindowInfoContext } from './WindowInfoProvider/context';
 export { default as WindowInfoProvider } from './WindowInfoProvider';
 export { default as withWindowInfo } from './withWindowInfo';
