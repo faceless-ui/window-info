0cb9ba2d1b6940329b5e7056b5a504f17f1a4117
Jacob Fletcher <jacobsfletch@gmail.com>
2020-02-21 13:45:32 -0500

    retrofits @trbl/utils into testing suite

--- src/index.test.js
+++ src/index.test.js
@@ -1,18 +1,8 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { mockWindowResizeEvents, mockRequestAnimationFrame } from '@trbl/utils';
 
 configure({ adapter: new Adapter() });
 
-window.resizeTo = function resizeTo(width, height) {
-  Object.assign(this, {
-    innerWidth: width,
-    innerHeight: height,
-    outerWidth: width,
-    outerHeight: height,
-  }).dispatchEvent(new this.Event('resize'));
-};
-
-window.requestAnimationFrame = (callback) => {
-  const timestamp = performance.now();
-  callback(timestamp);
-};
+mockWindowResizeEvents();
+mockRequestAnimationFrame();
