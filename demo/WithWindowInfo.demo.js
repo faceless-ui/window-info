0bdd00d01bfd7a1186699d44f3c032bf89c2a2d1
Jacob Fletcher <jacobsfletch@gmail.com>
2020-04-23 06:59:00 -0400

    builds WindowInfo component

--- demo/WithWindowInfo.demo.js
+++ demo/WithWindowInfo.demo.js
@@ -1,76 +1,16 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import { withWindowInfo } from '../src'; // swap '../src' for '../dist/build.bundle' to demo production build
+import PrintObject from './PrintObject';
 
 const WithWindowInfo = (props) => {
-  const {
-    windowInfo: {
-      width,
-      height,
-      breakpoints: {
-        xs, s, m, l, xl,
-      },
-      eventsFired,
-    },
-  } = props;
-
-  return (
-    <code>
-      <pre>
-        {'windowInfo: {'}
-        <br />
-        &emsp;
-        {`width: ${width},`}
-        <br />
-        &emsp;
-        {`height: ${height},`}
-        <br />
-        &emsp;
-        {'breakpoints: {'}
-        <br />
-        &emsp;&emsp;
-        {`xs: ${xs},`}
-        <br />
-        &emsp;&emsp;
-        {`s: ${s},`}
-        <br />
-        &emsp;&emsp;
-        {`m: ${m},`}
-        <br />
-        &emsp;&emsp;
-        {`l: ${l},`}
-        <br />
-        &emsp;&emsp;
-        {`xl: ${xl},`}
-        <br />
-        &emsp;
-        {'},'}
-        <br />
-        &emsp;
-        {`eventsFired: ${eventsFired},`}
-        <br />
-        {'}'}
-      </pre>
-    </code>
-  );
+  const { windowInfo } = props;
+  return PrintObject(windowInfo);
 };
 
 WithWindowInfo.defaultProps = {};
 
 WithWindowInfo.propTypes = {
-  windowInfo: PropTypes.shape({
-    width: PropTypes.number.isRequired,
-    height: PropTypes.number.isRequired,
-    breakpoints: PropTypes.shape({
-      xs: PropTypes.bool,
-      s: PropTypes.bool,
-      m: PropTypes.bool,
-      l: PropTypes.bool,
-      xl: PropTypes.bool,
-    }),
-    orientation: PropTypes.number,
-    eventsFired: PropTypes.number,
-  }).isRequired,
+  windowInfo: PropTypes.shape({}).isRequired,
 };
 
 export default withWindowInfo(WithWindowInfo);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { default as useWindowInfo } from './useWindowInfo';
+export { default as WindowInfo } from './WindowInfo';
 export { default as WindowInfoContext } from './WindowInfoProvider/context';
 export { default as WindowInfoProvider } from './WindowInfoProvider';
 export { default as withWindowInfo } from './withWindowInfo';
