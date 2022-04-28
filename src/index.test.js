7268cee1d27b36d404d38590859e8057e3f316df
Jacob Fletcher <jacobsfletch@gmail.com>
2022-04-28 15:48:09 -0400

    chore: updates dependencies - migrates from react-hot-loader to fast-refresh - updates react and react-dom peerDeps - deprecates enzyme - updates eslint and related deps

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,4 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import { mockWindowResizeEvents, mockRequestAnimationFrame } from './mocks';
-
-configure({ adapter: new Adapter() });
 
 mockWindowResizeEvents();
 mockRequestAnimationFrame();
